c68013f23416a92d82cc9749a1b07ed648a6b5f2
timofeya123 <timofeya123@inbox.ru>
2021-06-01 17:41:50 +0600

    SplashScreen_rect

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -1,8 +1,18 @@
 package com.AWT;
 
+import java.awt.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        SplashScreen splash = SplashScreen.getSplashScreen();
+        Rectangle bounds = splash.getBounds();
+        Graphics2D g2 = splash.createGraphics();
+        g2.setColor(Color.BLUE);
+        for (int i=0; i<=100; i++){
+            g2.fillRect(0,0, bounds.width*i/100, 30);
+            splash.update();
+            Thread.sleep(50);
+        }
     }
 }
