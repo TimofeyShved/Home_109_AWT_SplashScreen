6bbfad9f10cc73a330164f344140c7af0d0bd3b5
timofeya123 <timofeya123@inbox.ru>
2021-06-01 18:42:09 +0600

    SplashScreen_comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -14,43 +14,46 @@ import javax.swing.JPanel;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        SplashScreen splash = SplashScreen.getSplashScreen();
-        Rectangle bounds = splash.getBounds();
-        Graphics2D g2 = splash.createGraphics();
-        g2.setColor(Color.BLUE);
-        for (int i=0; i<=100; i++){
-            g2.fillRect(0,0, bounds.width*i/100, 30);
-            splash.update();
-            Thread.sleep(10);
+        SplashScreen splash = SplashScreen.getSplashScreen(); // достаём скрин загрузки из настроек программы
+        Rectangle bounds = splash.getBounds(); // создаём прямоугольник
+        Graphics2D g2 = splash.createGraphics(); // создаём графику на скрине
+        g2.setColor(Color.BLUE); // выбираем цвет синий
+        for (int i=0; i<=100; i++){ // цикл до 100
+            g2.fillRect(0,0, bounds.width*i/100, 30); // закрашиваем
+            splash.update(); // обновляем скрин
+            Thread.sleep(10); // задержка в 10 милисекунд
         }
 
-        final  JFrame splashFrame = new JFrame();
-        final JPanel splashPanel = new JPanel();
-        splashPanel.setLayout(new BoxLayout(splashPanel, BoxLayout.Y_AXIS));
+        // ------------------------------------------------------------------------- 2 вариант
 
-        final JProgressBar progressBar = new JProgressBar();
-        progressBar.setStringPainted(true);
-        progressBar.setSize(splash.getBounds().width, 20);
-        splashPanel.add(progressBar);
+        final  JFrame splashFrame = new JFrame(); // создаём форму
+        final JPanel splashPanel = new JPanel(); // создаём панель
+        splashPanel.setLayout(new BoxLayout(splashPanel, BoxLayout.Y_AXIS)); // устанавливаем вид отображения VBOX
 
-        final BufferedImage img = ImageIO.read(new File("1.jpg"));
-        JLabel imageLabel = new JLabel(new ImageIcon(img));
-        splashPanel.add(imageLabel).setBounds(splash.getBounds());
+        final JProgressBar progressBar = new JProgressBar(); // создаём прогресс
+        progressBar.setStringPainted(true); // отрисовка текста
+        progressBar.setSize(splash.getBounds().width, 20); // размеры
+        splashPanel.add(progressBar); // добавить на панель
 
-        splashFrame.add(splashPanel);
-        splashFrame.setBounds(splash.getBounds());
-        splashFrame.setVisible(true);
+        final BufferedImage img = ImageIO.read(new File("1.jpg")); // берем картинку
+        JLabel imageLabel = new JLabel(new ImageIcon(img)); // закидываем в метку
+        splashPanel.add(imageLabel).setBounds(splash.getBounds()); // а её в панель
+
+        splashFrame.add(splashPanel); // панель на форму
+        splashFrame.setBounds(splash.getBounds()); // размер формы
+        splashFrame.setVisible(true); // видимость
 
         try {
-            for (int i=0;i<100;i++){
-                progressBar.setString("Load: "+i);
+            for (int i=0;i<100;i++){ // цикл
+                progressBar.setString("Load: "+i); // прогресс
                 progressBar.setValue(i);
-                splashPanel.repaint();
-                Thread.sleep(20);
+                splashPanel.repaint(); // перерисовка
+                Thread.sleep(20); // задержка
             }
         }catch (InterruptedException e){
 
         }
-        splashFrame.setVisible(false);
+        splashFrame.setVisible(false); // видимость
+        System.exit(0);
     }
 }
